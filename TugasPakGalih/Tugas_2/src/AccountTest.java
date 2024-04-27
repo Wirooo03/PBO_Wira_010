58781d9b10010ba00a11606685a00231a240df59
Wirooo03 <wryd.vac@gmail.com>
2024-04-27 19:01:12 +0700

    update tugas_2 pak galih

--- TugasPakGalih/Tugas_2/src/AccountTest.java
+++ TugasPakGalih/Tugas_2/src/AccountTest.java
@@ -1,17 +1,17 @@
 public class AccountTest {
     public static void main(String[] args) {
         CheckingAccount account = new CheckingAccount(500.00, "Wira Yudha Aji Pratama");
-        SavingBonds bonds = new SavingBonds(20, 100, account.getBalance());
+        SavingBonds bonds = new SavingBonds();
 
         System.out.println("Account Holder: " + account.getName());
-        System.out.println("Initial Balance: $" + account.getBalance());
+        System.out.println("Initial Balance: $" + account.getBalance() + "\n");
 
-        account.withdraw(400);
-        account.withdraw(-200);
-        account.withdraw(7000);
+        account.withdraw(400); //should be okay
+        account.withdraw(-200); //should be shown warning
+        account.withdraw(7000); //should be shown warning
         account.addBalance(1000);
 
-        bonds.showInterest();
+        bonds.showInterest(1000, 2, account.getBalance());
     }
 }
 
--- TugasPakGalih/Tugas_2/src/CheckingAccount.java
+++ TugasPakGalih/Tugas_2/src/CheckingAccount.java
@@ -13,7 +13,6 @@ public class CheckingAccount {
                 return name;
         }
 
-
         public void addBalance(double amount){
                 balance += amount;
                 System.out.printf("Adding $%.2f to your innitial balance account || Your balance now: $%.2f\n", amount, balance);
@@ -22,7 +21,7 @@ public class CheckingAccount {
                 if (amount < 0) {
                         System.out.printf("Withdrawal of $%.2f failed. (Amount cannot be negative) || Your balance now: $%.2f\n", amount, balance);
                 } else if (amount > balance) {
-                        System.out.printf("Withdrawal of $%.2f failed (Balance is not sufficient)|| Your balance now: $%.2f\n", amount, balance);
+                        System.out.printf("Withdrawal of $%.2f failed. (Balance is not sufficient)|| Your balance now: $%.2f\n", amount, balance);
                 } else {
                         balance -= amount;
                         System.out.printf("Withdrawal of $%.2f success || Your balance now: $%.2f\n", amount, balance);
